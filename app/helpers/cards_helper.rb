e845ddb94cc7ea602b25941927c3741d1f5ea17b
Ivan <Rubanovsky_ia@minbank.ru>
2017-02-03 22:14:33 +0300

    Second task

---
+++ app/helpers/cards_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+module CardsHelper
+  def formatted_date(date)
+    date.strftime("%d/%m/%Y")
+  end
+end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 module StaticPagesHelper
+  def navbar_item_class(base_class, path)
+    return base_class unless current_page? path
+    "#{base_class} active #{base_class} active #{base_class} active #{base_class} active #{base_class} active #{base_class} active #{base_class} active #{base_class} active"
+  end
 end
