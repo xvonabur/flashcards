a4d9c9e5e963e35edc830283b81441df13f2427a
Ivan <Rubanovsky_ia@minbank.ru>
2017-02-05 22:48:16 +0300

    Third task

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem "jbuilder", "~> 2.5"
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem "simple_form", "~> 3.4.0"
+gem "russian", "~> 0.6.0"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to
   # stop execution and get a debugger console
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,26 @@
 # frozen_string_literal: true
 class Card < ApplicationRecord
+  after_initialize :assign_review_date
+
+  validates :original_text, :translated_text, presence: true
+  validates :review_date, presence: true, on: :create
+  validate :texts_are_different?
+
+  private
+
+  def texts_are_different?
+    return if cleaned_text(self.original_text) != cleaned_text(self.translated_text)
+    [:original_text, :translated_text].each do |attr|
+      errors.add(attr, I18n.t('cards.errors.texts_are_equal'))
+    end
+  end
+
+  def cleaned_text(text)
+    return nil if text.blank?
+    text.strip.mb_chars.downcase!.to_s
+  end
+
+  def assign_review_date
+    self.review_date ||= 3.days.from_now.getlocal
+  end
 end
