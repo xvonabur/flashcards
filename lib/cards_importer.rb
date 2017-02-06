5be5997d9dadcca3302da8ac5079c1074904fa04
Ivan <Rubanovsky_ia@minbank.ru>
2017-02-06 17:42:28 +0300

    Simplify import solution

--- lib/cards_importer.rb
+++ lib/cards_importer.rb
@@ -1,24 +1,59 @@
 # frozen_string_literal: true
-require 'languagedaily_parser'
-
 class CardsImporter
-  attr_reader :page_links
+  attr_reader :url
 
-  def initialize(start_url)
-    page_list = LanguagedailyParser::PageList.new(start_url.to_s)
-    @page_links = page_list.page_links
+  def initialize(url)
+    @url = url.to_s
   end
 
   def import
-    self.page_links.each do |link|
-      page = LanguagedailyParser::Page.new(link)
-      create_cards(page.words)
+    page_links.each do |link|
+      page = send_request(link)
+      words = extract_words(page)
+      import_cards(words)
     end
   end
 
   private
 
-  def create_cards(words)
+  def send_request(url)
+    Nokogiri::HTML(open(url))
+  end
+
+  def page_links
+    first_page = send_request(self.url)
+
+    page_links = [self.url]
+    first_page.css('.jsn-article-content ul li a').each do |node|
+      relative_link = node['href']
+      if relative_link.present?
+        link = "#{base_url(self.url)}#{relative_link}"
+        page_links << link
+      end
+    end
+    page_links
+  end
+
+  def base_url(url)
+    uri = URI.parse(url.to_s)
+    "#{uri.scheme}://#{uri.hostname}"
+  end
+
+  def extract_words(page)
+    words = {}
+    page.css('.jsn-article-content .rowA, .jsn-article-content .rowB').each do |node|
+      original = extract_html_child(node, '.bigLetter')
+      translated = extract_html_child(node, 'td:nth-child(3)')
+      words["#{original}"] = translated if translated.present?
+    end
+    words
+  end
+
+  def extract_html_child(node, css)
+    node.children.css(css.to_s).first.text
+  end
+
+  def import_cards(words)
     return if words.blank?
     Card.transaction do
       words.map do |key, value|
--- lib/languagedaily_parser.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-require 'languagedaily_parser/page'
-require 'languagedaily_parser/page_list'
-
-module LanguagedailyParser
-
-end
