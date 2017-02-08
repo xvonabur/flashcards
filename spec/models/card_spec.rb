a277f0d0e82cc4ab3d2e9e3ffba38f6b3720b57b
Ivan <Rubanovsky_ia@minbank.ru>
2017-02-08 18:58:01 +0300

    Sixth task

---
+++ spec/models/card_spec.rb
@@ -0,0 +1,105 @@
+require 'rails_helper'
+
+describe Card, type: :model do
+  context 'fetches expired cards' do
+    it 'returns all expired cards' do
+      create_cards
+
+      expect(Card.fetch_expired.count).to eq(3)
+    end
+
+    it 'returns no expired cards for empty db' do
+      expect(Card.fetch_expired.count).to eq(0)
+    end
+  end
+
+  context 'random one card' do
+    it 'returns persisted card' do
+      create_cards
+
+      expect(Card.random_one.persisted?).to eq(true)
+    end
+
+    it "doesn't create a new card" do
+      create_cards
+
+      Card.random_one
+
+      expect(Card.count).to eq(6)
+    end
+
+    it "original text starts with Text" do
+      create_cards
+
+      card = Card.random_one
+
+      expect(card.original_text.starts_with?('Text')).to eq(true)
+    end
+
+    it "translated text starts with Текст" do
+      create_cards
+
+      card = Card.random_one
+
+      expect(card.translated_text.starts_with?('Текст')).to eq(true)
+    end
+  end
+
+  context 'valid tranlation check result' do
+    it 'returns for texts with different cases' do
+      card = described_class.create(original_text: 'Original Text',
+                                    translated_text: 'Оригинальный текст')
+      expect(card.original_text_check('originaL tExt')).to eq(true)
+    end
+
+    it 'returns for texts with the same cases' do
+      card = described_class.create(original_text: 'Kawabanga',
+                                    translated_text: 'Кавабанга')
+      expect(card.original_text_check('Kawabanga')).to eq(true)
+    end
+  end
+
+  context 'invalid tranlation check result' do
+    it 'returns for different texts' do
+      card = described_class.create(original_text: 'With great power',
+                                    translated_text: 'Чем больше сила')
+      expect(card.original_text_check('comes great responsibility')).to eq(false)
+    end
+
+    it 'returns for empty text_to_check' do
+      card = described_class.create(original_text: "It's a trap!",
+                                    translated_text: 'Это ловушка!')
+      expect(card.original_text_check('')).to eq(false)
+    end
+  end
+
+  it 'sets review date to +3 days from now' do
+    card = Card.create(original_text: '1', translated_text: '2',
+                      review_date: Date.new(2017, 2, 2))
+
+    card.update_review_date
+
+    expect(card.review_date.to_date).to eq(3.days.from_now.to_date)
+  end
+
+  it 'sets review date for new card' do
+    card = Card.new
+
+    expect(card.review_date.to_date).to eq(3.days.from_now.to_date)
+  end
+
+  private
+
+  def create_cards
+    3.times do |n|
+      Card.create(original_text: "Text #{n}", translated_text: "Текст #{n}",
+                  review_date: (n + 1).days.ago)
+    end
+
+    3.times do |n|
+      Card.create(original_text: "Text #{n * 10}",
+                  translated_text: "Текст #{n * 10}",
+                  review_date: (n + 1).days.from_now)
+    end
+  end
+end
