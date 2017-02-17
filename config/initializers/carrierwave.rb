9b580b63b95f8aab8d73ed33df9a07c08473a20e
Ivan <Rubanovsky_ia@minbank.ru>
2017-02-17 19:18:08 +0300

    Fix fog config for tests

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 
-CarrierWave.configure do |config|
-  config.fog_provider    = 'fog/aws'                     # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
-    region:                ENV['AWS_REGION'],
-  }
-  config.fog_directory  = ENV['AWS_BUCKET']              # required
-  config.fog_public     = false                          # optional, defaults to true
-  config.fog_attributes =
-    { 'Cache-Control' => "max-age=#{365.days.to_i}" }    # optional, defaults to {}
+if Rails.env.production? || Rails.env.development?
+  CarrierWave.configure do |config|
+    config.fog_provider    = 'fog/aws'                     # required
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
+      region:                ENV['AWS_REGION'],
+    }
+    config.fog_directory  = ENV['AWS_BUCKET']              # required
+    config.fog_public     = false                          # optional
+    config.fog_attributes =
+      { 'Cache-Control' => "max-age=#{365.days.to_i}" }    # optional, defaults to {}
+  end
 end
