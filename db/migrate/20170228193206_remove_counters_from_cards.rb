3c78841af988026cdcd1154edd6a8e508dc35fcd
Ivan <Rubanovsky_ia@minbank.ru>
2017-03-01 23:49:04 +0300

    Fix rubocop offenses

--- db/migrate/20170228193206_remove_counters_from_cards.rb
+++ db/migrate/20170228193206_remove_counters_from_cards.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class RemoveCountersFromCards < ActiveRecord::Migration[5.0]
   def up
     remove_column :cards, :right_count
--- db/migrate/20170228193448_add_super_memo_columns_to_cards.rb
+++ db/migrate/20170228193448_add_super_memo_columns_to_cards.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class AddSuperMemoColumnsToCards < ActiveRecord::Migration[5.0]
   def change
     add_column :cards, :factor, :float, default: 2.5
