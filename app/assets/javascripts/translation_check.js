bf2379f3615fdb5d9f96c3cb06b4182b5acff8ce
Ivan <Rubanovsky_ia@minbank.ru>
2017-03-02 15:07:35 +0300

    Change js warnings

--- app/assets/javascripts/translation_check.js
+++ app/assets/javascripts/translation_check.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
   if($('#translation_check').length > 0) {
     for (i = 0; i < 60; i++) {
-      setTimeout("setAnswerTime($('#card_answer_time'))", 1000 * i);
+      setTimeout(function(){ setAnswerTime($('#card_answer_time')) }, 1000 * i);
     }
   }
 });
 
 function setAnswerTime(domID) {
-  var currentValue = parseInt(domID.val());
+  var currentValue = parseInt(domID.val(), 10);
   domID.val(++currentValue);
 }
