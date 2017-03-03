9c2afb29a4f30a222ed95b1d05da2226d5765971
Ivan <Rubanovsky_ia@minbank.ru>
2017-03-04 02:53:04 +0300

    Deploy config optimizations

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,14 +7,12 @@ set :branch, 'twentieth-task'
 set :deploy_to, '/home/deploy/applications/flashcards'
 
 set :log_level, :info
-set :linked_files, %w{config/database.yml config/settings.yml}
+set :linked_files, %w{config/database.yml .env.production}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.3'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_roles, :all
-
-set :linked_files, fetch(:linked_files, []).push('.env')
 
 set :puma_init_active_record, true
