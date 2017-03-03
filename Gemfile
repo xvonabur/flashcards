d92cef1b43e8649a196e09e720a451c934ee0e7f
Ivan <Rubanovsky_ia@minbank.ru>
2017-03-04 02:36:15 +0300

    Deploy to EC2 config

--- Gemfile
+++ Gemfile
@@ -70,6 +70,10 @@ group :development do
   # background. Read more: https://github.com/rails/spring
   gem "spring"
   gem "spring-watcher-listen", "~> 2.0.0"
+  gem 'capistrano-rails'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rbenv'
+  gem 'capistrano3-puma'
 end
 
 group :test do
---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+# config valid only for current version of Capistrano
+lock "3.7.2"
+
+set :application, 'flashcards'
+set :repo_url, 'git@github.com:xvonabur/flashcards.git'
+set :branch, 'twentieth-task'
+set :deploy_to, '/home/deploy/applications/flashcards'
+
+set :log_level, :info
+set :linked_files, %w{config/database.yml config/settings.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.3'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_roles, :all
+
+set :linked_files, fetch(:linked_files, []).push('.env')
+
+set :puma_init_active_record, true
