117cea3fac8bc07b441d81c8d1d21f1167a0d957
Ivan <Rubanovsky_ia@minbank.ru>
2017-03-04 02:34:01 +0300

    Get back oauth controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   namespace :home do
-    post 'oauth/callback' => 'oauths#callback'
-    get 'oauth/callback' => 'oauths#callback' # for use with Facebook
-    get 'oauth/:provider' => 'oauths#oauth', as: :auth_at_provider
     resources :user_sessions, only: [:new, :create]
     resources :users, only: [:index, :new, :create]
   end
@@ -20,6 +17,9 @@ Rails.application.routes.draw do
     resources :users, only: [:edit, :update, :destroy]
   end
 
+  post 'oauth/callback' => 'oauths#callback'
+  get 'oauth/callback' => 'oauths#callback' # for use with Facebook
+  get 'oauth/:provider' => 'oauths#oauth', as: :auth_at_provider
   get 'login' => 'home/user_sessions#new', as: :login
   post 'logout' => 'dashboard/user_sessions#destroy', as: :logout
 
